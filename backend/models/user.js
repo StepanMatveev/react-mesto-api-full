078416ef9b9df6a25bc39627d3166a24adaa9cc0
Stepan Matveev <stepa@MacBook-Pro-Stepan.local>
2021-03-01 16:08:46 +0300

    fix: review

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,21 +5,18 @@ const { isEmail } = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator(v) {
         // eslint-disable-next-line no-useless-escape
